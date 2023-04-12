7e476e6c90b3faa02f11bbee4e6a7a377708049f
Enoch Aniyikaye <97711219+Diggardson270@users.noreply.github.com>
2023-04-12 12:15:21 +0100

    Add files via upload

---
+++ interactive-rating-component/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  focus: ['responsive', 'hover', 'focus'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
